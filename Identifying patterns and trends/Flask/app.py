2c0ca83c0de7c9f99240c6f9a446db342d85e964
Suha271002 <128301923+Suha271002@users.noreply.github.com>
2023-04-13 14:31:07 +0530

    Add files via upload

---
+++ Identifying patterns and trends/Flask/app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import model
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+import sklearn as sk
+
+app = Flask(__name__,template_folder="templates")
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    age = request.args.get('age') 
+    gender = request.args.get('gender')
+    stream = request.args.get('stream')
+    internship = request.args.get('internship')
+    cgpa = request.args.get('cgpa')
+    backlogs = request.args.get('backlogs')
+    arr = np.array([gender,stream,internship,cgpa,backlogs])
+    brr = np.asarray(arr, dtype=float)
+    output = model.predict([brr])
+    if(output==1):
+        out = 'Yes'
+    else:
+        out = 'No'
+    return render_template('out.html', output=out)
+
+if __name__ == "__main__":
+    app.run(debug=True)
